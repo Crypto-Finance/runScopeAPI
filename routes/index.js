d9833db527d73d6c081aed13780e4ddd12f3a334
soft_dev_hugo_ <A01228679@exatec.tec.mx>
2022-09-10 13:48:26 -0500

    [MINOR]: defining routes and hbs renders

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Run Scope Modifier' });
 });
+
+router.post('/', (req, res, next) => {
+
+})
 
 module.exports = router;
