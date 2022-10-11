ec68630dbcf72ce2218f1dfde07c024d5c3f5861
hugozepeda18 <a01228679@exatec.tec.mx>
2022-10-10 19:56:41 -0500

    [MINOR]: Adding controllers to the architecture

---
+++ controllers/runScopeController.js
@@ -0,0 +1,24 @@
+import mainIntervalMapper from '../utils/mainIntervalHelpers.js'
+
+import customLogs from '../logging/logging.js'
+const log = customLogs.getLogger('runScopeController')
+
+async function createNewStrategy(req, res, next) {
+    const runScope = req.body
+    runScope.TP /= 100
+    runScope.SL /= 100
+    if(runScope.entryStrat === 'cross') {
+        runScope.entryPeriod = [3, 6, 50]
+    }
+    else {
+        runScope.entryPeriod = [200]
+    }
+    runScope.mainInterval = mainIntervalMapper[runScope.mainInterval]
+    runScope.entryPeriod = mainIntervalMapper[runScope.mainInterval]
+    runScope.exitPeriod = []
+    runScope.exitInterval = []
+    log.info(JSON.stringify(runScope))
+    res.send(runScope)
+}
+
+export default createNewStrategy
--- routes/runScopeRouter.js
+++ routes/runScopeRouter.js
@@ -1,5 +1,4 @@
 import runScopeHandler from '../MQTT/runScopeHandler.js';
-import mainIntervalMapper from '../utils/mainIntervalHelpers.js'
 
 import express from 'express'
 const router = express.Router();
@@ -7,27 +6,12 @@ const router = express.Router();
 import customLogs from '../logging/logging.js'
 const log = customLogs.getLogger('runScopeRouter')
 
-/* GET home page. */
+import createNewStrategy from '../controllers/runScopeController.js'
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Run Scope Modifier' });
 });
 
-router.post('/', (req, res, next) => {
-  const runScope = req.body
-  runScope.TP /= 100
-  runScope.SL /= 100
-  if(runScope.entryStrat === 'cross') {
-    runScope.entryPeriod = [3, 6, 50]
-  }
-  else {
-    runScope.entryPeriod = [200]
-  }
-  runScope.mainInterval = mainIntervalMapper[runScope.mainInterval]
-  runScope.entryPeriod = mainIntervalMapper[runScope.mainInterval]
-  runScope.exitPeriod = []
-  runScope.exitInterval = []
-  log.info(JSON.stringify(runScope))
-  res.send(runScope)
-})
+router.post('/', createNewStrategy)
 
 export default router;
